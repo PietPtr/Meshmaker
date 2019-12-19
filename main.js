7cef21d0751c5b21ca639570fa28852072148807
Pieter Staal <pieterstaal@outlook.com>
2019-12-19 17:39:32 +0100

    Remove tree implementation

--- main.js
+++ main.js
@@ -33,28 +33,6 @@ scene.add( directionalLight.target );
 
 
 
-var geometry = new THREE.PlaneGeometry( 1, 1, 1, 1 );
-geometry.rotateX(Math.PI / 2);
-var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide, wireframe: true} );
-var plane = new THREE.Mesh( geometry, material );
-// scene.add( plane );
-
-// let root = {corners: [0, 0, 0, 0], values: [1, 1, 1, 1, 2]};
-// let roottext = {corners: ['a', 'b', 'c', 'd'], values: ['ab', 'ac', 'bd', 'cd', 'x']}
-// let child0 = new Node(root, 1, 0);
-let root = new Root();
-root.createChildrenRecursively(1);
-root.children[0].createChildren();
-root.children[2].createChildren();
-root.children[0].children[0].createChildren();
-root.children[0].children[2].createChildren();
-// root.createChildrenRecursively(4);
-
-let group = new THREE.Group();
-console.log(group);
-root.createGeometry(group, []);
-scene.add(group);
-
 
 
 //////////////////////////////////////////////////////////////////////////////////
